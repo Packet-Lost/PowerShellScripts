528453106a63c29cdd1c1b14c25c5042f1afebc9
Packet-Lost <callen83@gmail.com>
2017-04-02 13:20:19 -0400

    Adding some SQS polling functions

---
+++ AWSFunctions/Start-SQSQueuePolling.ps1
@@ -0,0 +1,26 @@
+﻿Function Start-SQSQueuePolling{
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [String]$QueueUrl
+    )
+
+$numofmsg = Get-SQSQueueAttribute -QueueUrl $QueueUrl -AttributeName ApproximateNumberOfMessages
+
+    if ($numofmsg.ApproximateNumberOfMessages -gt 0){
+
+            Write-Output "Number of messages in the queue to process is approximately $($numofmsg.ApproximateNumberOfMessages)"
+
+            While ($numofmsg.ApproximateNumberOfMessages -gt 0) {
+            Start-SQSQueueProccessing -SQSQueueUrl $QueueUrl -Verbose
+            Start-Sleep -Seconds 5
+            $numofmsg = Get-SQSQueueAttribute -QueueUrl $QueueUrl -AttributeName ApproximateNumberOfMessages
+            Write-Output "Number of messages is $($numofmsg.ApproximateNumberOfMessages)"
+            }
+        }else{
+            Write-Output "No messages found in the queue to process"
+        }
+    Write-Output "Done processing SQS Messages in the Queue"
+}
---
+++ AWSFunctions/Start-SQSQueueProcessing.ps1
@@ -0,0 +1,43 @@
+﻿function Start-SQSQueueProccessing
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [String]$SQSQueueUrl
+    )
+
+    Process
+    {
+        try
+        {
+        Write-Verbose "$(Get-Date -Format u) Polling for $SQSQueueUrl messages"
+        $recmsg = Receive-SQSMessage -QueueUrl $SQSQueueUrl -MessageCount 10 -MessageAttributeName All
+        if ($recmsg) {
+            Write-Verbose "$(Get-Date -Format u) ... Messages found in queue...starting to process them"
+            $recmsg | ForEach-Object {
+                Write-Verbose "$(Get-Date -Format u) ... Processing message $($_.MessageId) that was $($_.Body)" 
+                $_.MessageAttributes | ForEach-Object {
+                ###This is where you can take action depending on the Key value of the message attribute###
+                    if ($_.Keys -eq "Enable") { Write-Output "We could do a Enable-Noun powershell cmdlet with the -parameter $($_.Enable.StringValue) here" }
+                    elseif ($_.Keys -eq "Disable") { Write-Output "We could do a Disable-Noun powershell cmdlet with the -parameter $($_.Disable.StringValue) here" }
+                    else { Write-Warning "Thats odd, the key of the message attribute was not what was expected so no action was taken" }
+                    }
+                Write-Verbose "$(Get-Date -Format u) Done processing $($_.MessageId) that was $($_.Body) attempting message deletion"  
+                Remove-SQSMessage -QueueUrl $SQSQueueUrl -ReceiptHandle $_.ReceiptHandle -Force
+                }
+
+        }else{ Write-Verbose "$(Get-Date -Format u) ... No messages were pulled from the queue...nothing to do right now"}
+
+        }
+        catch {
+            Write-Error $_
+        }
+
+    }
+
+}
